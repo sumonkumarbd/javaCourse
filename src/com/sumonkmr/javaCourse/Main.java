b226af742f54fe2a6216b85b3e4d37d13dcadeb5
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-08 23:02:28 +0600

    Hello This is all java source code

---
+++ src/com/sumonkmr/javaCourse/Main.java
@@ -0,0 +1,9 @@
+package com.sumonkmr.javaCourse;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+    }
+}
