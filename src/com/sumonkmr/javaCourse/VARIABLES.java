29aa3de0e3fb40b3f0d2fd2637da8ddb407f275a
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-08 23:12:33 +0600

    In this class I learn what is the variables and literals

---
+++ src/com/sumonkmr/javaCourse/VARIABLES.java
@@ -0,0 +1,13 @@
+package com.sumonkmr.javaCourse;
+
+public class VARIABLES {
+    public static void main(String[] args) {
+        String name = "Sumonkmr";
+        byte age = 25;
+        long phn = 8801841177525L;
+        double gpa = 4.21d;
+        String address = "Ishwardi Pabna";
+        System.out.print(name+" "+age+" "+phn+" "+gpa+" "+address);
+
+    }
+}
