0072c2694797e459b51c04136027000c1c20d0cc
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-08 23:26:42 +0600

    In this class I learn what is the variables and literals

--- src/com/sumonkmr/javaCourse/LITERALS.java
+++ src/com/sumonkmr/javaCourse/LITERALS.java
@@ -1,2 +1,26 @@
-package com.sumonkmr.javaCourse;public class LITERALS {
+package com.sumonkmr.javaCourse;
+
+public class LITERALS {
+    public static void main(String[] args) {
+        //        code here
+        int age = 25;
+        byte b1 = 7;
+        short s1 = 6;
+        long l1 = 11;
+        float f1 = 1;
+        double d1 = 2.3;
+        char c1 = 'A';
+        String name = "Sumonkmr";
+        boolean bol = true;
+
+        System.out.println(age);
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(name);
+        System.out.println(bol);
+    }
 }
