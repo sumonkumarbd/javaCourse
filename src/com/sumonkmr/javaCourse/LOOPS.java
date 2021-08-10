abe607b6f9c5f82fb05fc564ecd3aadf0e20c7f6
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-10 23:13:18 +0600

    In this time I've build a logic into do while loop

--- src/com/sumonkmr/javaCourse/LOOPS.java
+++ src/com/sumonkmr/javaCourse/LOOPS.java
@@ -8,6 +8,18 @@ public class LOOPS {
         for (int i = 0; i <= 10; i++){
             System.out.println(i+" Hi This is Sumon kmr in for loop");
         } // --For Loop end
-        
+
+
+        // --Do while loop
+        int j = 1;
+
+        do {
+            System.out.println("Hi This is Sumon kmr in do-while loop");
+            break;
+
+        }while (j == 10);{
+            System.out.println("First time the code will run then stop for false conditions");
+
+        } // --Do while loop end
     }
 }
