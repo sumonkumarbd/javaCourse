754763fa4adc908c15cfa893734a58f19ed085ee
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-10 23:11:06 +0600

    In this class I've learn for loop in deep conversession

---
+++ src/com/sumonkmr/javaCourse/LOOPS.java
@@ -0,0 +1,13 @@
+package com.sumonkmr.javaCourse;
+
+public class LOOPS {
+    public static void main(String[] args) {
+
+
+        // -- For loop
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i+" Hi This is Sumon kmr in for loop");
+        } // --For Loop end
+        
+    }
+}
