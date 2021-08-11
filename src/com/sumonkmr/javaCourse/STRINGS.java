d2c83e1b588ca5bf1187b914bc4e089f714b784f
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-11 23:32:16 +0600

    This is a Strings

---
+++ src/com/sumonkmr/javaCourse/STRINGS.java
@@ -0,0 +1,8 @@
+package com.sumonkmr.javaCourse;
+
+public class STRINGS {
+    public static void main(String[] args) {
+        String name = "Sumon";
+        System.out.printf("My nam is %s",name);
+    }
+}
