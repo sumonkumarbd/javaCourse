399472805cca9b0532f0af2cd3abf88cb046bc91
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-11 23:38:16 +0600

    This is toLowerCase String Method

--- src/com/sumonkmr/javaCourse/STRINGS.java
+++ src/com/sumonkmr/javaCourse/STRINGS.java
@@ -1,8 +1,16 @@
 package com.sumonkmr.javaCourse;
 
+import java.util.Locale;
+
 public class STRINGS {
     public static void main(String[] args) {
         String name = "Sumon";
-        System.out.printf("My nam is %s",name);
+       // System.out.printf("My nam is %s",name);
+
+        // --Strings All methods
+
+        // -- toLowerCase method
+        String lname = name.toLowerCase(Locale.ROOT);
+        System.out.println(lname);
     }
 }
