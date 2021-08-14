748d941b6f359c221534e5908a55178876619495
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-14 23:42:11 +0600

    This is an Example Of Method

---
+++ src/com/sumonkmr/javaCourse/Method.java
@@ -0,0 +1,17 @@
+package com.sumonkmr.javaCourse;
+
+public class Method {
+
+    // --Write method here
+    static int sum(int x,int y){
+        int z;
+        z = x + y;
+        return z;
+
+    }
+    public static void main(String[] args) {
+        int result = sum(3,5);
+        System.out.printf("This is an Example of How to use Method. And The Result is : %d",result);
+
+    }
+}
