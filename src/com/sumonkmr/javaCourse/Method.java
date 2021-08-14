8a625bf3f283c507dd4ab7c56a9ba6ef6391f651
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-15 01:16:54 +0600

    Here I'm making object and of invocation

--- src/com/sumonkmr/javaCourse/Method.java
+++ src/com/sumonkmr/javaCourse/Method.java
@@ -16,15 +16,27 @@ public class Method {
     static int menus (int x,int y){
         int z = x - y;
         return z;
-    }
+    } // --End menus method
 
+    // -- method invocation
+    int invocation(int x , int y){
+        int result = x * y;
+        return result;
+    } // -- end method invocation
 
 
     public static void main(String[] args) {
-        int sresult = sum(3,5);
-        System.out.printf("This is an Example of How to use Method. And The Result is : %d %n" ,sresult);
-        int mresult = menus(30,25);
-        System.out.printf("This is an Example of How to use Method.This case I'm using here menus oparation And The Result is : %d",mresult);
+//        int sresult = sum(3,5);
+//        System.out.printf("This is an Example of How to use Method. And The Result is : %d %n" ,sresult);
+//        int mresult = menus(30,25);
+//        System.out.printf("This is an Example of How to use Method.This case I'm using here menus oparation And The Result is : %d",mresult);
+
+        // -- making object of invocation
+        Method mobj = new Method();
+        int a = 10;
+        int b = 5;s
+        int res = mobj.invocation(a , b);
+        System.out.printf("This is an example of invocation or creat object of class. And the result is : %d %n ",res);
 
     }
 }
