e5e20b7df176f46c4019be87d3509ec32ce5da30
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-14 23:55:07 +0600

    This is an menus Oparation to using method

--- src/com/sumonkmr/javaCourse/Method.java
+++ src/com/sumonkmr/javaCourse/Method.java
@@ -3,15 +3,28 @@ package com.sumonkmr.javaCourse;
 public class Method {
 
     // --Write method here
+
+    // -- sum Method
     static int sum(int x,int y){
         int z;
         z = x + y;
         return z;
 
+    } // end sum Method
+
+    // Start menus Method
+    static int menus (int x,int y){
+        int z = x - y;
+        return z;
     }
+
+
+
     public static void main(String[] args) {
-        int result = sum(3,5);
-        System.out.printf("This is an Example of How to use Method. And The Result is : %d",result);
+        int sresult = sum(3,5);
+        System.out.printf("This is an Example of How to use Method. And The Result is : %d %n" ,sresult);
+        int mresult = menus(30,25);
+        System.out.printf("This is an Example of How to use Method.This case I'm using here menus oparation And The Result is : %d",mresult);
 
     }
 }
