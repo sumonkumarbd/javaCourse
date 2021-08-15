3981bbfe59b100345e76166b5b5dff098cf86826
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-16 00:16:27 +0600

    Here an another example for changing value of array using method

--- src/com/sumonkmr/javaCourse/MethodOverLoading.java
+++ src/com/sumonkmr/javaCourse/MethodOverLoading.java
@@ -10,6 +10,11 @@ public class MethodOverLoading {
         a = 20;
     } // --end change1
 
+    // -- change2
+    static void change2(int [] arr){
+        arr [0] = 88;
+    } // -- end change2
+
 
     public static void main(String[] args) {
         // -- Executable code
@@ -17,9 +22,14 @@ public class MethodOverLoading {
        // msg();
 
         // -- Changing int
-        int x = 70;
+       /* int x = 70;
         change1(x);
-        System.out.println("So x is :"+x);
+        System.out.println("So x is :"+x);*/    // --int type is not to be change by methods
+
+        // -- changing array
+        int [] marks = {56,77,43,32};
+        change2(marks);
+        System.out.println(marks[0]);  // -- array has can change value by methods
 
 
 
