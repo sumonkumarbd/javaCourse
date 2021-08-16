cec9356889311fb65c6efc6d2799da63c7b686d6
sumonkmr <sumonkmr.dev@gmail.com>
2021-08-17 00:07:12 +0600

    here I've assign same name of tow methods with diffarent parameters

--- src/com/sumonkmr/javaCourse/MethodOverLoading.java
+++ src/com/sumonkmr/javaCourse/MethodOverLoading.java
@@ -15,6 +15,18 @@ public class MethodOverLoading {
         arr [0] = 88;
     } // -- end change2
 
+    // -- methode overloding
+    //we can assign same name of meny methodes but they have to pass deffarent parameters.
+    static int sum(int a, int b){
+        return a+b;
+
+    }// --end sum
+
+    static int sum(int a, int b, int c){
+        return a+b+c;
+
+    }// --end sum
+
 
     public static void main(String[] args) {
         // -- Executable code
@@ -27,9 +39,13 @@ public class MethodOverLoading {
         System.out.println("So x is :"+x);*/    // --int type is not to be change by methods
 
         // -- changing array
-        int [] marks = {56,77,43,32};
+       /* int [] marks = {56,77,43,32};
         change2(marks);
-        System.out.println(marks[0]);  // -- array has can change value by methods
+        System.out.println(marks[0]);  // -- array has can change value by methods*/
+
+        // -- calling sum method
+        System.out.println("The result is : "+ sum(10, 8));
+        System.out.println("The result is : "+ sum(10, 8, 4)); //we can assign same name of meny methodes but they have to pass deffarent parameters.
 
 
 
